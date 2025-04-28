4c7df689d0aaee1eca98e5381a19c10137a0988a
GOBLIN HOLE <168582609+pinkpipe@users.noreply.github.com>
2025-04-28 22:01:18 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,38 +1,32 @@
 #!/bin/bash
 
-# Определение версии драйвера NVIDIA
-NV_LIB=$(find /usr/lib/modules -name nvidia.ko | grep $(uname -r) | head -1)
-if [ -z "$NV_LIB" ]; then
-    echo "ERROR: NVIDIA kernel module not found!"
+NV_VER=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader | head -1 | cut -d. -f1)
+if [ -z "$NV_VER" ]; then
+    echo "ERROR: NVIDIA driver version not found!"
     exit 1
 fi
-NV_VER=$(modinfo "$NV_LIB" | grep ^version | awk '{print $2}' | cut -d. -f1)
 
-# Настройка данных
 DATA_FOLDER=$(realpath "${1:-./data}")
 mkdir -p "$DATA_FOLDER"
 
-# Подключение камер
 CAMERA_DEVICES=()
 for dev in /dev/video*; do
     [ -e "$dev" ] && CAMERA_DEVICES+=("--device=$dev:$dev")
 done
 
-# Разрешение X11
 xhost +local:docker
 
-# Сборка образа
 docker build . -t deepfacelive --build-arg NV_VER="$NV_VER" || {
     echo "Docker build failed!"
     exit 1
 }
 
-# Запуск контейнера
 docker run \
     --ipc host \
     --gpus '"device=0"' \
     -e DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
+    -e QT_QPA_PLATFORM=xcb \
     -e __GLX_VENDOR_LIBRARY_NAME=mesa \
     -e VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/radeon_icd.x86_64.json \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
@@ -41,5 +35,4 @@ docker run \
     --rm -it \
     deepfacelive
 
-# Отмена разрешений
 xhost -local:docker
